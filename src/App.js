6f5ac455d65fe4c50646f8db1b8e430186a6b9dc
Artsiom Voitas <artsiom.voitas@gmail.com>
2022-08-21 21:05:13 +0300

    feat(app): created app using react with github api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Main from "./components/Main";
+import "./App.scss";
+import RepositoryCard from "./components/RepositoryCard/index";
+import SearchQuery from "./components/SearchQuery/index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        <Route path="/search">
+          <SearchQuery />
+        </Route>
+        <Route path="/repository/:username/:repositoryName">
+          <RepositoryCard />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
