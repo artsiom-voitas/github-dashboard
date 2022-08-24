4adcc5756080c5510b7e4df9a83475741d0dbde0
Artsiom Voitas <artsiom.voitas@gmail.com>
2022-08-24 17:49:15 +0300

    feat(App): added React.memo and reduced imports

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
-import Main from "./components/Main";
-import "./App.scss";
-import RepositoryCard from "./components/RepositoryCard/index";
-import SearchQuery from "./components/SearchQuery/index";
+import React from 'react'
+import {
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import Main from './components/Main'
+import './App.scss'
+import RepositoryCard from './components/RepositoryCard/'
+import SearchQuery from './components/SearchQuery/'
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/" exact>
-          <Main />
-        </Route>
-        <Route path="/search">
-          <SearchQuery />
-        </Route>
-        <Route path="/repository/:username/:repositoryName">
-          <RepositoryCard />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </div>
-  );
-}
+const App = React.memo(
+    function App() {
+      return (
+          <div className="App">
+            <Switch>
+              <Route path="/" exact>
+                <Main />
+              </Route>
+              <Route path="/search">
+                <SearchQuery />
+              </Route>
+              <Route path="/repository/:username/:repositoryName">
+                <RepositoryCard />
+              </Route>
+              <Redirect to="/" />
+            </Switch>
+          </div>
+      )
+    },
+)
 
 export default App;
