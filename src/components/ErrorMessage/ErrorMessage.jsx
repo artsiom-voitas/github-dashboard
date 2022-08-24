a8dbe7aec124421e3a59b022eb9e33e6f5737ec0
Artsiom Voitas <artsiom.voitas@gmail.com>
2022-08-24 18:16:40 +0300

    fix(ErrorMessage): inline styles have been replaced - reduced imports - used React.memo - added new classes

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,44 +1,36 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { Alert } from "react-bootstrap";
-import errorImage from "../../images/error.png";
-import Header from "../Header/index";
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Alert } from 'react-bootstrap'
+import errorImage from '../../images/error.png'
+import Header from '../Header/'
 
-function ErrorMessage(props) {
-  const { message = "Please, try to reload your page." } = props;
-  return (
-    <div>
-      <Header />
-      <div
-        style={{
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        <img
-          src={errorImage}
-          alt="ErrorMessage"
-          className="w-50 d-flex mx-auto mb-5"
-        />
-        <Alert
-          variant="danger"
-          style={{
-            minWidth: 320,
-            textAlign: "center",
-          }}
-        >
-          Oops, something went wrong.
-          <div>{message}</div>
-        </Alert>
-      </div>
-    </div>
-  );
-}
+const ErrorMessage = React.memo(
+    function ErrorMessage(props) {
+        const { message = 'Please, try to reload your page.' } = props
+        return (
+            <div>
+                <Header />
+                <div className="position-absolute start-50 top-50 transform-translate-50">
+                    <img
+                        src={errorImage}
+                        alt="Sad face"
+                        className="w-50 d-flex mx-auto mb-5"
+                    />
+                    <Alert
+                        variant="danger"
+                        className="text-center min-w-320"
+                    >
+                        Oops, something went wrong.
+                        <div>{message}</div>
+                    </Alert>
+                </div>
+            </div>
+        )
+    },
+)
 
 ErrorMessage.propTypes = {
-  message: PropTypes.string,
-};
+    message: PropTypes.string,
+}
 
-export default ErrorMessage;
+export default ErrorMessage
