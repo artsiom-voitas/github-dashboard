213302b0062f41463064743facedeaf2c062a14a
Artsiom Voitas <artsiom.voitas@gmail.com>
2022-08-24 18:48:39 +0300

    fix(Header): added useCallback and React.memo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,53 +1,68 @@
-import React, { useState } from "react";
-import { InputGroup, Form, Button } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import logo from "../../images/github-logo.svg";
-import "./header.scss";
-import { getRepositoriesBySearchQuery } from "../../redux/searchRepositoriesReducer/thunk";
+import React, {
+    useCallback,
+    useState,
+} from 'react'
+import {
+    InputGroup,
+    Form,
+    Button,
+} from 'react-bootstrap'
+import {
+    useDispatch,
+    useSelector,
+} from 'react-redux'
+import { Link } from 'react-router-dom'
+import logo from '../../images/github-logo.svg'
+import './header.scss'
+import { getRepositoriesBySearchQuery } from '../../redux/searchRepositoriesReducer/thunk'
 
-function Header(_props)
-{
-  const [searchValue, setSearchValue] = useState('')
-  const dispatch                      = useDispatch()
-  const currentPage                   = useSelector(
-      (state) => state.searchRepositories.currentPage,
-  )
+const Header = React.memo(
+    function Header() {
+        const [searchValue, setSearchValue] = useState('')
+        const dispatch                      = useDispatch()
+        const currentPage                   = useSelector(
+            (state) => state.searchRepositories.currentPage,
+        )
 
-  function onInputChange(event)
-  {
-    setSearchValue(event.target.value)
-  }
+        const handleChange = useCallback(
+            (event) => {
+                setSearchValue(event.target.value)
+            },
+            [],
+        )
 
-  function searchHandler() {
-    dispatch(getRepositoriesBySearchQuery(searchValue, currentPage))
-  }
-  const searchPlaceholder = "Search or jump to...";
-  return (
-    <header>
-      <Link to="/">
-        <img className="logo" src={logo} alt="Github logo" />
-      </Link>
-      <InputGroup className="search mb-3">
-        <Form.Control
-          aria-label="Text input with dropdown button"
-          type="text"
-          placeholder={searchPlaceholder}
-          className="search__input"
-          value={searchValue}
-          onChange={onInputChange}
-        />
-        <Link
-          to={`/search?query=${searchValue}&page=${currentPage}`}
-          className="ms-2"
-        >
-          <Button variant="primary" onClick={searchHandler}>
-            Search
-          </Button>
-        </Link>
-      </InputGroup>
-    </header>
-  );
-}
+        const handleSearch = useCallback(
+            () => {
+                dispatch(getRepositoriesBySearchQuery(searchValue, currentPage))
+            },
+            [searchValue, currentPage, dispatch],
+        )
 
-export default Header;
+        return (
+            <header>
+                <Link to="/">
+                    <img className="logo" src={logo} alt="Github logo" />
+                </Link>
+                <InputGroup className="search mb-3">
+                    <Form.Control
+                        type="text"
+                        placeholder="Search or jump to..."
+                        className="search__input"
+                        value={searchValue}
+                        onChange={handleChange}
+                    />
+                    <Link
+                        to={`/search?query=${searchValue}&page=${currentPage}`}
+                        className="ms-2"
+                    >
+                        <Button variant="primary" onClick={handleSearch}>
+                            Search
+                        </Button>
+                    </Link>
+                </InputGroup>
+            </header>
+        )
+    },
+)
+
+export default Header
