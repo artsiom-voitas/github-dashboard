b955500ee3e172518786d40657972d13d2c60844
Artsiom Voitas <artsiom.voitas@gmail.com>
2022-09-03 19:51:46 +0300

    fix(SearchQuery): added useMemo and simplified part of component

--- src/components/SearchQuery/SearchQuery.jsx
+++ src/components/SearchQuery/SearchQuery.jsx
@@ -1,5 +1,5 @@
 import * as queryString from 'query-string';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Alert } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
@@ -12,10 +12,10 @@ import SearchPagination from '../SearchPagination/';
 const SearchQuery = React.memo(function SearchQuery() {
   const dispatch = useDispatch();
   const { search } = useLocation();
-  const { query, page } = queryString.parse(search);
+  const { query, page } = useMemo(() => queryString.parse(search), [search]);
   const pageNumber = Number(page);
 
-  const repos = useSelector((state) => state.searchRepositories.items);
+  const repositories = useSelector((state) => state.searchRepositories.items);
   const totalCount = useSelector((state) => state.searchRepositories.totalCount);
 
   const isLoading = useSelector((state) => state.searchRepositories.isLoading);
@@ -26,15 +26,14 @@ const SearchQuery = React.memo(function SearchQuery() {
 
   return (
     <div>
-      {!repos ? (
+      {!repositories ? (
         <ErrorMessage />
       ) : (
         <div>
           <Header />
           {totalCount === 0 && !isLoading ? (
             <Alert variant="warning" className="text-center my-5">
-              {/* eslint-disable-next-line react/no-unescaped-entities */}
-              We haven't found anything on your request:
+              {"We haven't found anything on your request:"}
               <span className="main__title--bold"> {query} </span>
               <div>Please try again.</div>
             </Alert>
@@ -44,8 +43,8 @@ const SearchQuery = React.memo(function SearchQuery() {
                 Look what we found on your request:
                 <span className="main__title--bold"> {query} </span>
               </div>
-              <RepositoriesList repositories={repos} isLoading={isLoading} />
-              {!isLoading ? <SearchPagination query={query} currentPage={pageNumber} /> : null}
+              <RepositoriesList repositories={repositories} isLoading={isLoading} />
+              {!isLoading && <SearchPagination query={query} currentPage={pageNumber} />}
             </div>
           )}
         </div>
